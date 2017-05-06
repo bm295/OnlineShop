321b7ed3a4817dbcb7014231589d59c51f06f082
bm295 <baominh.nguyen.295@gmail.com>
2017-05-06 22:58:34 +0800

    commit OnlineShop

---
+++ OnlineShop/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Admin/Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
