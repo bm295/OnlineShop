c5efe7884b74d8bad719b079467dd9e137139017
bm295 <baominh.nguyen.295@gmail.com>
2017-05-07 17:20:19 +0800

    add entity framework for login purpose

---
+++ OnlineShop/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Admin/Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
